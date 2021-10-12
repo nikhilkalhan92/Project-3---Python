afdce9299af101a394ea50ae09fd34e2341f2c1a
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2021-10-12 20:49:37 +0000

    Wire up API's and make sure access to google sheets

--- run.py
+++ run.py
@@ -6,3 +6,17 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
     ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('wrestling')
+
+sales = SHEET.worksheet('sales')
+
+data = sales.get_all_values()
+
+print(data)
+
+
+

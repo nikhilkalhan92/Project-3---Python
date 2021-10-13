dd952443394f99116887be03678c913fb1ffdd46
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2021-10-13 19:53:38 +0000

    add pick a date function to application

--- run.py
+++ run.py
@@ -13,10 +13,37 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('wrestling')
 
 sales = SHEET.worksheet('sales')
+stock = SHEET.worksheet('stock')
+profit = SHEET.worksheet('profit')
 
-data = sales.get_all_values()
+sales_data = sales.get_all_values()
+stock_data = stock.get_all_values()
+profit_data = profit.get_all_values()
 
-print(data)
+def get_days_sales_data(date: str):
+    """method to get sales data for a specific day"""
+    headings = []
+    data = []
+    for i, row in enumerate(sales_data):
+        if i == 0: 
+            headings = row
+        else:
+            if row[0] == date:
+                data = row
+    for i, item in enumerate(headings):
+        if i > 0:
+            print(item, data[i])
 
 
 
+def main():
+    """this is the main executiable function"""
+    print("some instructions")  #write a series of print function to explain to the user what the application does
+   
+   
+    print("pick a date")
+    date = input("")
+    #do some form of data validation
+    get_days_sales_data(date)
+
+main()

f00fabaea9ba26ca30171c4a6e8a5a8832f3bddb
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2021-10-16 20:31:23 +0000

    adjusted run.py name of folder

--- run.py
+++ run.py
@@ -11,6 +11,11 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('data')
+
 
 class Sales_insight:
 
